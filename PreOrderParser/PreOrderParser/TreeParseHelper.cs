1e9818cfe18011a396f195a5c3ad1e815f734f89
Sas <saswata66@gmail.com>
2021-02-07 20:01:48 +1100

    Adding the test and the library project

---
+++ PreOrderParser/PreOrderParser/TreeParseHelper.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PreOrderParser
+{
+    public class TreeParseHelper
+    {
+        public TreeParseHelper()
+        {
+        }
+
+        //internal bool IsValidPreOrderTraverssedNodeList(List<int> preOrderNodeList)
+        //{
+        //    if (preOrderNodeList.Count == 0)
+        //        return true;
+
+        //    if (IsSigleNodeList(preOrderNodeList))
+        //        return true;
+
+        //    var currentRoot = preOrderNodeList.First();
+        //    var nextNodeInList = preOrderNodeList.ElementAt(1);
+
+        //    var visitedRootsIndexInNodeList = new Stack<int>();
+        //    visitedRootsIndexInNodeList.Push(0);
+        //    while(nextNodeInList <= currentRoot)
+        //    {   
+        //        if (preOrderNodeList.Count == visitedRootsIndexInNodeList.Peek() + 1) // No further nodes in list
+        //            break;
+        //        currentRoot = nextNodeInList;
+        //        nextNodeInList = preOrderNodeList.ElementAt(visitedRootsIndexInNodeList.Peek() + 1);
+        //        visitedRootsIndexInNodeList.Push(visitedRootsIndexInNodeList.Peek() + 1);
+        //    }
+
+        //    // find the correct parent for nextNodeInList by popping the stack
+        //    while (nextNodeInList > visitedRootsIndexInNodeList.Peek())
+        //    {
+        //        var lastroot = visitedRootsIndexInNodeList.Pop();
+        //    }   
+
+        //    return false;
+        //}
+
+        public bool IsValidPreOrderTraverssedNodeList(List<int> preOrderNodeList)
+        {
+            if (preOrderNodeList.Count == 0 || preOrderNodeList.Count == 1)
+                return true;
+
+            var startingNode = preOrderNodeList.First();
+            var leftTraversalMinimumValues = new List<int>();
+            for(var index = 1; index < preOrderNodeList.Count; ++index)
+            {
+                var currentNodeValue = preOrderNodeList.ElementAt(index);
+                if (leftTraversalMinimumValues.Where(nodeValue => currentNodeValue <= nodeValue).Any())
+                    return false;
+                
+                if (currentNodeValue <= startingNode)
+                    startingNode = currentNodeValue;
+                else
+                {
+                    leftTraversalMinimumValues.Add(startingNode);
+                    startingNode = currentNodeValue;
+                }   
+            }
+
+            return true;
+        }
+    }
+}
---
+++ PreOrderParser/TreeParserTests/PreOrderParserTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using PreOrderParser;
+using FluentAssertions;
+
+namespace TreeParserTests
+{
+    public class PreOrderParserTests
+    {
+        public static IEnumerable<object[]> ValidNodeLists()
+        {
+            yield return new object[] { };
+            yield return new object[] { 10, 10, 10, 10 };
+            yield return new object[] { 10, 12 };
+            yield return new object[] { 10, 7, 4, 2, 6, 9, 21, 18, 20, 44 };
+        }
+
+        public static IEnumerable<object[]> InValidNodeLists()
+        {
+            yield return new object[] { 10, 7, 4, 2, 6, 9, 21, 5, 1, 44 };
+            yield return new object[] { 10, 7, 4, 2, 6, 9, 21, 5, 1, 44 };
+        }
+
+        [Theory]
+        [InlineData(new int[] { })]
+        [InlineData(new int[] { 10, 10, 10, 10 })]
+        [InlineData(new int[] { 10, 12 })]
+        [InlineData(new int[] { 10, 7, 4, 2, 6, 9, 21, 18, 20, 44 })]
+        [InlineData(new int[] { 10, 12, 20, 25, 30 })]
+        [InlineData(new int[] { 10 })]
+        [InlineData(new int[] { 10, 7, 4, 2, 6, 9, 8, 21, 5, 1, 44 })]
+        public void ListOfNodes_In_Correct_Order_Should_Be_Validated(int[] preorderNodes)
+        {
+            var treeParseHelper = new TreeParseHelper();
+            treeParseHelper.IsValidPreOrderTraverssedNodeList(preorderNodes.ToList()).Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData(new int[] { 10, 7, 4, 2, 9, 6, 21, 5, 1, 44 })]
+        [InlineData(new int[] { 10, 7, 4, 2, 6, 9, 8, 21, 5, 1, 44 })]
+        public void ListOfNodes_In_InCorrect_Order_Should_Not_Be_Validated(int[] preorderNodes)
+        {
+            // better name later
+            var treeParseHelper = new TreeParseHelper();
+            treeParseHelper.IsValidPreOrderTraverssedNodeList(preorderNodes.ToList()).Should().BeFalse();
+        }
+    }
+}
